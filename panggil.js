dd83981739de49557372217d212991f3ea2eebb9
x7x7x7x <nolongga@gmail.com>
2018-04-05 00:24:43 +0700

    Update panggil.js

--- panggil.js
+++ panggil.js
@@ -24,7 +24,7 @@ var xxx=null;
 if(x=="1"){xxx="10-best-forex-strategies.html"}
 if(x=="2"){xxx="mesothelioma-compensation.html"}
 if(x=="3"){xxx="claiming-compensation-for-mesothelioma.html"}
-if(x=="4"){xxx="/mesothelioma-benign-or-malignant.html"}a_to_ve[i].href="http://kesafelinks.blogspot.com/2018/04/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="/mesothelioma-benign-or-malignant.html"}a_to_ve[i].href="https://kesafelinks.blogspot.com/2018/04/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
